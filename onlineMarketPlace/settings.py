8876a3c31bf4622d3aa4edf2afffaeae231ee5cb
Ivan Necerini <ivan.necerini@gmail.com>
2023-06-06 22:45:20 +0200

    Update settings.py

--- onlineMarketPlace/settings.py
+++ onlineMarketPlace/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!!c$%6wb^9!x)e*6nk98d1yaf%1d4=tfmriw5q56x%jbie@f4s'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
